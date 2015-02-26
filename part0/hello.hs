c6336136efd349e1b7e966b01819278900ce6d11
Alexey Kotlyarov <a@koterpillar.com>
2015-02-26 19:11:30 +1100

    Use forever

--- part0/hello.hs
+++ part0/hello.hs
@@ -3,12 +3,11 @@ import Network
 
 main = do
   socket <- listenOn (PortNumber 1234)
-  loop socket
+  forever $ loop socket
 
 loop socket = do
   (connection,_,_) <- accept socket
   hPutStr connection "hello world\r\n"
   hFlush connection
   hClose connection
-  loop socket
 
